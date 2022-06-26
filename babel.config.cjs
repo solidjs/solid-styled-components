28cc2b71950c286d2b0dad46122d17dcad97a55f
Joshua Van Deren <livetoscout@gmail.com>
2022-06-25 23:37:06 -0600

    Added "solid" preset to babel config

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     test: {
-      presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"]
+      presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript", "solid"]
     }
   }
 };
